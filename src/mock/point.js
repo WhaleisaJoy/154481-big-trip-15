1c5587f692eccbf716cbd9af15adaf25b1e25001
WhaleisaJoy <afterlifelineonomega@gmail.com>
2021-08-21 15:40:46 +0300

    Создает моковые данные и встраивает их в шаблон

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,79 +1,66 @@
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
+import { getRandomInteger, shuffle } from "../utils";
+import { generateDestination } from "./destination";
+import { generateOffers } from "./offer";
+import { TYPES } from "../const";
+import dayjs from "dayjs";
 
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
+const generateFromDate = () => {
+  const maxGap = 60000 * 60 *24 * 7;
+  const gap = getRandomInteger(-maxGap, maxGap);
 
-// https://learn.javascript.ru/task/shuffle
-const shuffle = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-};
+  return dayjs().add(gap, 'ms');
+}
 
-const generateType = () => {
-  const types = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
-  const randomType = getRandomInteger(0, types.length - 1);
-
-  return types[randomType];
-};
-
-const generateDestinationName = () => {
-  const destinations = ['Amsterdam', 'Chamonix', 'Geneva', 'Ankara', 'Moscow', 'Milan', 'Barcelona', 'Beijing', 'Beirut'];
-  const randomDestination = getRandomInteger(0, destinations.length - 1);
-
-  return destinations[randomDestination];
-};
-
-const generateDestinationDescription = () => {
-  const MAX_SIZE = 5;
-  const descriptions = [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    'Cras aliquet varius magna, non porta ligula feugiat eget.',
-    'Fusce tristique felis at fermentum pharetra.',
-    'Aliquam id orci ut lectus varius viverra.',
-    'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
-    'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
-    'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
-    'Sed sed nisi sed augue convallis suscipit in sed felis.',
-    'Aliquam erat volutpat.',
-    'Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.',
+const generateToDate = (dateFrom) => {
+  const durationOptions = [
+    {
+      //lessThanHour
+      minGap: 15,
+      maxGap: 60,
+    },
+    {
+      //lessThanOneDay
+      minGap: 61,
+      maxGap: 60 * 24,
+    },
+    {
+      //moreThanOneDay
+      minGap: 60 * 24 + 1,
+      maxGap: 60 * 24 * 3 + 1,
+    },
   ];
 
-  const copyOfDescriptions = [...descriptions];
-  shuffle(copyOfDescriptions);
-  const randomDescriptionLength = getRandomInteger(0, MAX_SIZE);
-  const description = copyOfDescriptions.slice(0, randomDescriptionLength).join(' ');
+  const randomOption = getRandomInteger(0, durationOptions.length - 1);
+  const gap = getRandomInteger(durationOptions[randomOption].minGap, durationOptions[randomOption].maxGap);
 
-  return description;
+  return dayjs(dateFrom).add(gap, 'minute');
+}
+
+const generateType = () => {
+  const randomType = getRandomInteger(0, TYPES.length - 1);
+
+  return TYPES[randomType];
 };
 
-const generatePics = () => {
-  const picsURL = 'http://picsum.photos/248/152?r=';
-  const randomPicsAmount = getRandomInteger(0, 10);
-  const pics = [];
-
-  for (let i = 0; i < randomPicsAmount; i++) {
-    const randomPicNum = getRandomInteger(0, 10);
-    pics.push(picsURL + randomPicNum);
-  }
-
-  return pics;
+const generatePrice = () => {
+  return getRandomInteger(10, 500);
 };
+
+
 
 export const generatePoint = () => {
   const type = generateType();
+  const dateFrom = generateFromDate();
+  const dateTo = generateToDate(dateFrom);
 
   return {
+    dateFrom,
+    dateTo,
     icon: `${type}.png`,
     type,
-    destination: {
-      name: generateDestinationName(),
-      description: generateDestinationDescription(),
-      pics: generatePics(),
-    },
+    destination: generateDestination(),
+    basePrice: generatePrice(),
+    offers: generateOffers(type),
+    isFavorite: Boolean(getRandomInteger(0, 1)),
   };
 };
--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,13 +1,54 @@
-export const createTripInfoTemplate = () => (
-  `<section class="trip-main__trip-info  trip-info">
-    <div class="trip-info__main">
-      <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+import dayjs from "dayjs";
 
-      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+const createRouteTemplate = (points) => {
+  const destinationNames = points.map((point) => point.destination.name);
+
+  const reducer = (accumulator, currentValue) => accumulator + ` &mdash; ` + currentValue;
+
+  const tripRoute = destinationNames.length > 3
+    ? `${destinationNames[0]} &mdash; ... &mdash; ${destinationNames[destinationNames.length - 1]}`
+    : destinationNames.reduce(reducer);
+
+  return tripRoute;
+}
+
+const createDatesTemplate = (points) => {
+  const firstPoint = points[0];
+  const lastPoint = points[points.length - 1];
+
+  const startDate = dayjs(firstPoint.dateFrom).format('MMM DD');
+  const endDate = dayjs(lastPoint.dateFrom).isSame(dayjs(firstPoint.dateFrom), 'month')
+    ? dayjs(lastPoint.dateFrom).format('DD')
+    : dayjs(lastPoint.dateFrom).format('MMM DD');
+
+  return {startDate, endDate};
+}
+
+const createCostValueTemplate = (points) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  const prices = points.map((point) => point.basePrice);
+  const costValue = prices.reduce(reducer);
+
+  return costValue;
+}
+
+
+
+export const createTripInfoTemplate = (points) => {
+  const route = createRouteTemplate(points);
+  const dates = createDatesTemplate(points);
+  const costValue = createCostValueTemplate(points);
+
+  return `<section class="trip-main__trip-info  trip-info">
+    <div class="trip-info__main">
+      <h1 class="trip-info__title">${route}</h1>
+
+      <p class="trip-info__dates">${dates.startDate}&nbsp;&mdash;&nbsp;${dates.endDate}</p>
     </div>
 
     <p class="trip-info__cost">
-      Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">${costValue}</span>
     </p>
-  </section>`
-);
+  </section>`;
+};
